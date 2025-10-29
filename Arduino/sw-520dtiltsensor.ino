e0c41610737290ab093451e87ebaa2d059cfea89
Clara Jenhee <clara20franco@gmail.com>
2025-10-29 17:06:45 -0600

    Add tilt sensor with LED and buzzer

---
+++ Arduino/sw-520dtiltsensor.ino
@@ -0,0 +1,23 @@
+const int sensor = 2;
+const int ledPin = 13;
+const int buzzer = 10;
+
+int sensorState = 0;
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+
+  pinMode(sensor, INPUT);
+}
+
+void loop() {
+  sensorState = digitalRead(sensor);
+
+  if(sensorState == HIGH){
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(buzzer, LOW);
+  }else {
+    digitalWrite(buzzer, HIGH);
+    digitalWrite(ledPin, LOW);
+  }
+}

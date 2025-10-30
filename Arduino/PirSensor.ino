d80abfa1516d2b1440a1b27ec2799b079978e8a0
Clara Jenhee <clara20franco@gmail.com>
2025-10-29 18:10:01 -0600

    Alarm with PIR sensor functionality with LEDs and buzzer

---
+++ Arduino/PirSensor.ino
@@ -0,0 +1,26 @@
+int pir = 2;
+int ledGreen = 3; 
+int ledRojo = 4;
+int buzz = 5;
+int estado = 0;
+
+void setup(){
+  pinMode(pir, INPUT);
+  pinMode(ledVerde, OUTPUT);
+  pinMode(ledRojo, OUTPUT);
+  pinMode(buzz, OUTPUT);
+  delay(20000);
+}
+void loop(){
+  estado = digitalRead(pir);
+  if(estado == HIGH){
+    digitalWrite(ledRojo, HIGH);
+    digitalWrite(ledVerde, LOW);
+    digitalWrite(buzz, HIGH);
+    delay(4000);
+  }else{
+    digitalWrite(ledRojo, LOW);
+    digitalWrite(ledVerde, HIGH);
+    digitalWrite(buzz, LOW);
+  }
+}

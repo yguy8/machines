9d3d14b7a992a8ed5795f3ad3f41a4fcb0005671
Clara Jenhee <clara20franco@gmail.com>
2025-10-29 21:14:18 -0600

    Update PIR sensor code with new pin configuration
    Refactor PIR sensor code to use new pin assignments and improve structure.

--- Arduino/PirSensor.ino
+++ Arduino/PirSensor.ino
@@ -1,26 +1,31 @@
-int pir = 2;
-int ledVerde = 3; 
-int ledRojo = 4;
-int buzz = 5;
-int estado = 0;
+int ledPin = 12; // declaramos el led en el pin 12 del arduino
+int sensorPin=11; // declaramos el sensor PIR en el pin 11
+int buzz = 10;
+int val = 0; //variable para asignar la lectura del sensor PIR
 
-void setup(){
-  pinMode(pir, INPUT);
-  pinMode(ledVerde, OUTPUT);
-  pinMode(ledRojo, OUTPUT);
-  pinMode(buzz, OUTPUT);
-  delay(20000);
+void setup()
+{
+pinMode(ledPin, OUTPUT); 
+pinMode(sensorPin, INPUT);
+pinMode(buzz, OUTPUT);
+Serial.begin(9600);
+for(int i = 0; i > 30; i++) //Utilizamos un for para calibrar el sensor 
+{
+delay(1000);
 }
-void loop(){
-  estado = digitalRead(pir);
-  if(estado == HIGH){
-    digitalWrite(ledRojo, HIGH);
-    digitalWrite(ledVerde, LOW);
-    digitalWrite(buzz, HIGH);
-    delay(4000);
-  }else{
-    digitalWrite(ledRojo, LOW);
-    digitalWrite(ledVerde, HIGH);
-    digitalWrite(buzz, LOW);
-  }
+delay(50);
+}
+void loop()
+{
+val = digitalRead(sensorPin); //Lee el valor de la variable (val)
+if (val == HIGH) //Si detecta que hay movimiento manda activar el led que hay conectado en el pin 12 del arduino
+{
+digitalWrite(ledPin, HIGH);
+digitalWrite(buzz, HIGH);
+}
+else //Si la condición anterior no se cumple manda apagar el led
+{
+digitalWrite(ledPin, LOW);
+digitalWrite(buzz, LOW);
+}
 }

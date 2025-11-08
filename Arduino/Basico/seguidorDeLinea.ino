e5a05542e62e1c091cc62b12be23f1dda47f083a
Clara Jenhee <clara20franco@gmail.com>
2025-11-08 15:30:50 -0700

    Update seguidorDeLinea.ino

--- Arduino/Basico/seguidorDeLinea.ino
+++ Arduino/Basico/seguidorDeLinea.ino
@@ -5,10 +5,19 @@ void setup() {
 }
 
 void loop() {
+  //salida digital 
   if(digitalRead(sensorPin) == HIGH){
     Serial.println("Va en la dirección correcta");
   }else{
     Serial.println("Salio fuera de la linea negra");
   }
   delay(50);
+
+  //salida pin análogo
+  // cuando no es la linea negra el valor es aprox 25 y cuando hay algo negro sube de entre 300-800
+  
+  //int valorAnalogo = analogRead(0);
+  //Serial.print("El valor análogo es: ");
+  //Serial.println(valorAnalogo);
+  //delay(200);
 }

8c54ff3a225dc47ce712856dc79db6d1603af8d0
Clara Jenhee <clara20franco@gmail.com>
2025-11-08 16:07:06 -0700

    Create sensorVibracionAlertaBasica.ino

---
+++ Arduino/Basico/sensorVibracionAlertaBasica.ino
@@ -0,0 +1,26 @@
+const int sensorPin = 9;
+int buzz = 8;
+int ledPin = 7;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(sensorPin, INPUT);
+  pinMode(buzz, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  int valor = 0;
+  valor = digitalRead(sensorPin);
+
+  if(valor == HIGH){
+    //Serial.println("Estado normal");
+    digitalWrite(ledPin, 0);
+    digitalWrite(buzz, 0);
+  }else{
+    //Serial.println("Movimiento detectado");
+    digitalWrite(ledPin, 1);
+    digitalWrite(buzz, 1);
+  }
+  //delay(500);
+}

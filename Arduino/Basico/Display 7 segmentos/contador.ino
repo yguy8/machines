4d55e60d4007dd2648f3a208f365f855b789f1cf
Clara Jenhee <clara20franco@gmail.com>
2025-11-13 16:47:07 -0700

    Add 7-segment display counter functionality
    Implement a simple counter for a 7-segment display using the SevSeg library.

---
+++ Arduino/Basico/Display 7 segmentos/contador.ino
@@ -0,0 +1,25 @@
+#include "SevSeg.h"
+SevSeg sevseg;
+
+void setup() {
+    byte numDigits = 1;
+    byte digitPins[] = {2}; // pin común del dígito
+    byte segmentPins[] = {6,5,2,3,4,7,8,9}; // A,B,C,D,E,F,G,DP
+//pines de arriba  de izquierda a derecha 8, 7, 6, 5
+//pines de abajo de izquierda a derecha 4, 3, 2, 9
+    bool resistorsOnSegments = true;
+
+    byte hardwareConfig = COMMON_CATHODE; 
+    sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments);
+    sevseg.setBrightness(50);
+}
+
+void loop() {
+    for (int i = 0; i < 10; i++) {
+        sevseg.setNumber(i);
+        unsigned long start = millis();
+        while (millis() - start < 1000) {
+            sevseg.refreshDisplay(); // refrescar constantemente
+        }
+    }
+}

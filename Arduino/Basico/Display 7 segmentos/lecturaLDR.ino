5043fed827c5f6ae2f7be0a3c2dd169932d2a512
Clara Jenhee <clara20franco@gmail.com>
2025-11-17 09:35:03 -0700

    Add LDR reading and buzzer functionality
    Implement LDR reading and display on 7-segment.

---
+++ Arduino/Basico/Display 7 segmentos/lecturaLDR.ino
@@ -0,0 +1,38 @@
+#include "SevSeg.h"
+SevSeg sevseg;
+
+// Pin del buzzer
+const int buzzerPin = A0;   // salida digital
+const int ldrPin = A1;      // entrada analógica para la LDR
+
+void setup() {
+  byte numDigits = 4;
+  byte digitPins[] = {10, 11, 12, 13};
+  byte segmentPins[] = {9, 2, 3, 5, 6, 8, 7, 4};
+
+  bool resistorsOnSegments = true; 
+  bool updateWithDelaysIn = true;
+  byte hardwareConfig = COMMON_ANODE; 
+
+  sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments);
+  sevseg.setBrightness(50);
+
+  pinMode(buzzerPin, OUTPUT); // buzzer como salida
+}
+
+void loop() {
+  sevseg.refreshDisplay();
+
+  // Leer valor de la LDR
+  int valorLuz = analogRead(ldrPin); // 0-1023
+
+  // Mostrar el valor en el display
+  sevseg.setNumber(valorLuz);
+
+  // Si está oscuro (umbral ajustable)
+  if (valorLuz < 200) { 
+    tone(buzzerPin, 2000, 200); // pitido de 200ms
+  } else {
+    noTone(buzzerPin);
+  }
+}

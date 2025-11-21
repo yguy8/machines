b0f2d1606104f3b42fe9c676be597d4fd4d16530
Clara Jenhee <clara20franco@gmail.com>
2025-11-20 17:11:48 -0700

    Update katcomm2.ino

--- Arduino/Basico/RGBCatodocomun/katcomm2.ino
+++ Arduino/Basico/RGBCatodocomun/katcomm2.ino
@@ -1,19 +1,15 @@
 //para cambiar colores
-
-
 int rPin = 5;
 int gPin = 6;
 int bPin = 7;
 
 void setup() {
-  // put your setup code here, to run once:
 pinMode(rPin, OUTPUT);
 pinMode(gPin, OUTPUT);
 pinMode(bPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   analogWrite(rPin, 255);
   analogWrite(gPin, 120);
   analogWrite(bPin, 90);
